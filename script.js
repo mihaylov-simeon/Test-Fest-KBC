e6c2a46a8a9b1ccbb4cc263cfe5b9d318c585ca8
SSM <mihaylov.design@gmail.com>
2023-04-04 14:37:40 +0300

    initial-commit

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-function safiaFunctionallity() {
+    document.addEventListener('DOMContentLoaded', () => {
+        const loginBtn = document.querySelector('body > header > button');
     
-}
+        loginBtn.addEventListener('click', loginBtnHandler);
+    
+        function loginBtnHandler() {
+            console.log('TEST');
+        }
+    });
