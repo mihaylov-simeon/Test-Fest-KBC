0b60649cd1e7e80bd9e65f19b33669fde04438de
SSM <mihaylov.design@gmail.com>
2023-04-06 14:05:17 +0300

    back-to-top-button

--- script.js
+++ script.js
@@ -50,7 +50,27 @@
         }
     });
 
+// Get the button:
+let topBtn = document.getElementById("topBtn");
 
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+    scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 1500 || document.documentElement.scrollTop > 1500) {
+    topBtn.style.display = "block";
+  } else {
+    topBtn.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
     
   
 
