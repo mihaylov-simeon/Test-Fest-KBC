6dec119ac8547595a0a27b1eae1950e1c9a0b701
S. M <75495411+mihaylov-simeon@users.noreply.github.com>
2023-04-07 13:47:45 +0300

    Update animated-scroll.js

--- animated-scroll.js
+++ animated-scroll.js
@@ -1,11 +1,8 @@
-window.onscroll = function() {
-    onScrollFunction()
-};
+window.onscroll = function() {myFunction()};
 
-function onScrollFunction() {
+function myFunction() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
-  const element = document.getElementById("scroll-tracker");
-  element.style.width = scrolled + "%";
+  document.getElementById("myBar").style.width = scrolled + "%";
 }
