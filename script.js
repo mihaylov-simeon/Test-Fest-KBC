75a6aae9476756a59594efecbe839efde7ecdfcd
SSM <mihaylov.design@gmail.com>
2023-04-08 10:43:18 +0300

    design-changes

--- script.js
+++ script.js
@@ -1,77 +1,88 @@
     
     
-    document.addEventListener('DOMContentLoaded', () => {
-        const loginBtn = document.querySelector('body > header > button');
-        const geStartedTrialBtn = document.querySelector('#features > div.trial > button');
-        const priceCardBtnOne = document.querySelector('#pricing > div.price-card-wrapper > div:nth-child(1) > button');
-        const priceCardBtnTwo = document.querySelector('#pricing > div.price-card-wrapper > div:nth-child(2) > button');
-        const priceCardBtnThree = document.querySelector('#pricing > div.price-card-wrapper > div:nth-child(3) > button');
-        const getStarterTrialTwoBtn = document.querySelector('#pricing > div.trial-pricing > button');
+// Selectors and event listeners    
+document.addEventListener('DOMContentLoaded', () => {
+    const loginBtn = document.querySelector('body > header > button');
+    const geStartedTrialBtn = document.querySelector('#features > div.trial > button');
+    const priceCardBtnOne = document.querySelector('#pricing > div.price-card-wrapper > div:nth-child(1) > button');
+    const priceCardBtnTwo = document.querySelector('#pricing > div.price-card-wrapper > div:nth-child(2) > button');
+    const priceCardBtnThree = document.querySelector('#pricing > div.price-card-wrapper > div:nth-child(3) > button');
+    const getStarterTrialTwoBtn = document.querySelector('#pricing > div.trial-pricing > button');
+    const topBtn = document.getElementById("topBtn");
+
+    loginBtn.addEventListener('click', loginBtnHandler);
+    geStartedTrialBtn.addEventListener('click', getStartedTrialBtnHandler)
+    priceCardBtnOne.addEventListener('click', priceCardOneHandler)
+    priceCardBtnTwo.addEventListener('click', priceCardTwoHandler)
+    priceCardBtnThree.addEventListener('click', priceCardThreeHandler)
+    getStarterTrialTwoBtn.addEventListener('click', getStartedTrialTwoHandler)
+
+// functionality
+    function loginBtnHandler() {
+        window.location.href = 'login-form.html'
+    }
     
-        loginBtn.addEventListener('click', loginBtnHandler);
-        geStartedTrialBtn.addEventListener('click', getStartedTrialBtnHandler)
-        priceCardBtnOne.addEventListener('click', priceCardOneHandler)
-        priceCardBtnTwo.addEventListener('click', priceCardTwoHandler)
-        priceCardBtnThree.addEventListener('click', priceCardThreeHandler)
-        getStarterTrialTwoBtn.addEventListener('click', getStartedTrialTwoHandler)
+    function priceCardOneHandler() {
+        window.location.href = 'login-form.html'
+    }
 
-        function loginBtnHandler() {
-            window.location.href = 'login-form.html'
-        }
-        
-        function priceCardOneHandler() {
-            window.location.href = 'login-form.html'
-        }
+    function getStartedTrialBtnHandler() {
+        window.location.href = 'login-form.html'
+    }
 
-        function getStartedTrialBtnHandler() {
-            window.location.href = 'login-form.html'
-        }
+    function priceCardTwoHandler() {
+        window.location.href = 'login-form.html'
+    }
 
-        function priceCardTwoHandler() {
-            window.location.href = 'login-form.html'
-        }
+    function priceCardThreeHandler() {
+        window.location.href = 'login-form.html'
+    }
 
-        function priceCardThreeHandler() {
-            window.location.href = 'login-form.html'
-        }
+    function getStartedTrialTwoHandler() {
+        window.location.href = 'login-form.html'
+    }
+});
 
-        function getStartedTrialTwoHandler() {
-            window.location.href = 'login-form.html'
-        }
-    });
+// booking consultation form redirect
+document.addEventListener('DOMContentLoaded', () => {
+    const bookConsultationBtn = document.querySelector('#welcome-heading > div > a > button');
 
-    document.addEventListener('DOMContentLoaded', () => {
-        const bookConsultationBtn = document.querySelector('#welcome-heading > div > a > button');
-    
-        bookConsultationBtn.addEventListener('click', bookingBtnHandler);
-    
-        function bookingBtnHandler() {
-            window.location.href = 'consultation-form.html'
-        }
-    });
+    bookConsultationBtn.addEventListener('click', bookingBtnHandler);
 
-// Get the button:
-let topBtn = document.getElementById("topBtn");
+    function bookingBtnHandler() {
+        window.location.href = 'consultation-form.html'
+    }
+});
+
+
+
 
 // When the user scrolls down 1500px from the top of the document, show the button
 window.onscroll = function() {
-    scrollFunction()
+scrollFunction();
+scrollBarTracker();
 };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 1500 || document.documentElement.scrollTop > 1500) {
-    topBtn.style.display = "block";
-  } else {
-    topBtn.style.display = "none";
-  }
+if (document.body.scrollTop > 1500 || document.documentElement.scrollTop > 1500) {
+topBtn.style.display = "block";
+} else {
+topBtn.style.display = "none";
+}
 }
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+document.body.scrollTop = 0; // For Safari
+document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
-    
-  
 
-    
+function scrollBarTracker() {
+var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+var scrolled = (winScroll / height) * 100;
+document.getElementById("myBar").style.width = scrolled + "%";
+}
+
+
+
